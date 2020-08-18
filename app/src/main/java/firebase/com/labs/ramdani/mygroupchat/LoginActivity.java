0da435a35cecf1080b44324b0266c6a32f9396f5
Rakesh Parmar <57476311+RDParmarKnitlife@users.noreply.github.com>
2020-08-18 19:24:41 +0530

    Update LoginActivity.java

--- app/src/main/java/firebase/com/labs/ramdani/mygroupchat/LoginActivity.java
+++ app/src/main/java/firebase/com/labs/ramdani/mygroupchat/LoginActivity.java
@@ -2,9 +2,8 @@ package firebase.com.labs.ramdani.mygroupchat;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -13,10 +12,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
